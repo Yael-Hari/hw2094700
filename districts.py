1bd9d494501978642e1782c85f81da9a9020d1e3
YaelHari <yaelhhari@gmail.com>
2021-04-26 11:14:20 +0300

    left for yael: -argvs shite -docstring -main -imports -init Districts

--- districts.py
+++ districts.py
@@ -1,7 +1,14 @@
-class Districts:
-    def __init__(dataset):
-        pass
 
+
+class Districts:
+    # __init__
 
     def filter_districts(self, letters):
-        pass
+        # todo docstring
+        all_districts_list = self.dataset.get_all_districts()
+        relevant_districts_list = []
+        for name in all_districts_list:
+            if name[0] in letters:
+                relevant_districts_list.append(name)
+
+        self.dataset.set_districts_data(districts=relevant_districts_list)
--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-import sys
+def Q1(path):
+    my_data = Data(path=path)
+    my_districts = Districts(dataset=my_data)
+    d_letters_set = {'S', 'L'}
+    my_districts.filter_districts(d_letters_set)
+    features = ['hospitalized_with_symptoms',
+                'intensive_care',
+                'total_hospitalized',
+                'home_insulation']
 
+    my_districts.print_details(features, statistics.stats_list)
 
-def main():
-    pass
-
-
-def Q1():
-    pass
-
-
-def Q2():
-    pass
-
-
-if __name__ == '__main__':
-    main (sys.argv)
